de78610b42df3dceae2be28be3db51f33cee523b
IN007848 <Reshma@oppo2019>
2019-12-11 18:13:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/retrofit_demo/Destination.kt
@@ -0,0 +1,5 @@
+package com.example.retrofit_demo
+
+data class Destination(var email:String?=null,
+                       var password:String?=null,
+                       var confirmPassword:String?=null)
---
+++ app/src/main/java/com/example/retrofit_demo/MainActivity.kt
@@ -0,0 +1,125 @@
+package com.example.retrofit_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.retrofit_demo.services.DestinationService
+import com.example.retrofit_demo.services.ServiceBuilder
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+//https://www.youtube.com/watch?v=TC68Fw8ZlhM&list=PLlxmoA0rQ-LzEmWs4T99j2w6VnaQVGEtR&index=23
+//https://www.journaldev.com/13639/retrofit-android-example-tutorial
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //loadDestinations()
+        // loadDestinations1()
+
+        button.setOnClickListener {
+
+            val newDestination = Destination()
+            newDestination.email = text_email.text.toString()
+            newDestination.password = text_password.toString()
+            newDestination.confirmPassword = text_confirm.toString()
+
+            val destinationService: DestinationService = ServiceBuilder.builService(DestinationService::class.java)
+
+            val requestcall = destinationService.addUserdetails(newDestination)
+
+            requestcall.enqueue(object : Callback<Destination>
+            {
+                override fun onFailure(call: Call<Destination>?, t: Throwable?)
+                {
+                    Toast.makeText(this@MainActivity, "failed to retrivexx items", Toast.LENGTH_SHORT).show()
+                    Log.e("oppo","thorowable"+ t?.localizedMessage)
+                }
+
+                override fun onResponse(call: Call<Destination>?, response: Response<Destination>) {
+                    if (response.isSuccessful)
+                    {
+                        Toast.makeText(this@MainActivity, "sucesss", Toast.LENGTH_SHORT).show()
+                    }
+                    else {
+                        Toast.makeText(this@MainActivity, "failed to retrive items",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+
+
+            })
+        }
+
+
+    }}
+
+
+
+/* private fun loadDestinations()
+    {
+       val destinationService:DestinationService=ServiceBuilder.builService(DestinationService::class.java)
+
+
+
+        val requestcall:Call<List<Destination>> = destinationService.getDestinationlist()//
+
+        // (_>call Destinationservice ->RETURN Call objects- fetch destination from server
+
+        requestcall.enqueue(object :Callback<List<Destination>> {
+
+            override fun onFailure(call: Call<List<Destination>>?, t: Throwable?) {
+
+          Toast.makeText(this@MainActivity,"failed to retrive items",Toast.LENGTH_SHORT).show()
+      }
+
+     override fun onResponse(call: Call<List<Destination>>?, response: Response<List<Destination>>?)
+       {
+        if (response!!.isSuccessful)
+        {
+        val destinationlist:List<Destination> =response.body()
+
+            //destnyrecycler.adapter=DestinationAdapter(destinationlist)
+        }
+      }
+
+
+    })
+}
+    private fun loadDestinations1()
+    {
+        val destinationService:DestinationService=ServiceBuilder.builService(DestinationService::class.java)
+
+        //val requestcall:Call<List<Destination>> = destinationService.getDestinationlist()//(_>call Destinationservice ->RETURN Call objects- fetch destination from server
+
+        val requestcall:Call<Destination> = destinationService.getDestinationid(id = 1)
+
+        requestcall.enqueue(object :Callback<Destination> {
+            override fun onFailure(call: Call<Destination>?, t: Throwable?) {
+                Toast.makeText(this@MainActivity,"failed to retrive items",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(call: Call<Destination>?, response: Response<Destination>?) {
+
+                if (response!!.isSuccessful)
+                {
+                    val destination:Destination =response.body()
+                    destination.let {
+                       // email.setText( destination.email)
+                       // pwd.setText( destination.password)
+
+                    }
+
+                }
+
+            }
+
+        })
+}}*/
